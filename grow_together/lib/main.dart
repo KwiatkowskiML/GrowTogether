fcff4051b8540b6a7962f5dbed6d24eccb5e8805
Łukasz Kryczka <lukasz.kryczka000@gmail.com>
2024-09-29 01:21:57 +0200

    Reverted to default main

--- grow_together/lib/main.dart
+++ grow_together/lib/main.dart
@@ -3,8 +3,7 @@ import 'package:grow_together/widgets/event_popup_card/event_popup_card.dart';
 import 'package:grow_together/widgets/map.dart';
 
 void main() {
-  // runApp(GrowTogetherApp());
-  runApp(const TestApp());
+  runApp(GrowTogetherApp());
 }
 
 class GrowTogetherApp extends StatelessWidget {
@@ -18,39 +17,6 @@ class GrowTogetherApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const HomeScreen(),
-    );
-  }
-}
-
-class TestApp extends StatelessWidget {
-  const TestApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Google Maps App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(title: const Text('Event Popup Test')),
-        body: const Padding(
-          padding: EdgeInsets.all(16.0),
-          child: EventPopupCard(
-            avatarInitial: 'A',
-            eventTitle: 'Charity Run',
-            eventOwnerName: 'John Doe',
-            eventOwnerEmail: 'john.doe@gmail.com',
-            eventDescription:
-                'Join us for a charity run to raise funds for cancer research. Every contribution counts!',
-            assembledAmount: 1423,
-            totalGoalAmount: 2000,
-            growersCount: 242,
-            benefitsText:
-                'You will get a 10% discount on all products. Your contribution helps us make a difference!',
-          ),
-        ),
-      ),
     );
   }
 }
