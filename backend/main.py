daec5c10ee916bee5eab7dde2a4c64dd5db53c0c
Jlisowskyy <Jlisowskyy@gmail.com>
2024-09-28 22:24:57 +0200

    Sketched backend dir

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,7 @@
 from fastapi import FastAPI
-
+from models import EventModel
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
+@app.get("/events/add")
+async def say_hello(model: EventModel):
     return {"message": f"Hello {name}"}
