b05e4e1d7f7730cd672fc500f29ac8f14481ae58
Łukasz Kryczka <lukasz.kryczka000@gmail.com>
2024-09-29 02:33:32 +0200

    Revert "Refactoring"
    This reverts commit 35f658830dc4d7fa87cac6176afbed87ffaedc2f.

--- grow_together/test/event_popup_card_test.dart
+++ grow_together/test/event_popup_card_test.dart
@@ -2,7 +2,6 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:grow_together/widgets/event_popup_card/event_popup_card.dart';
-import 'package:grow_together/widgets/greek_vine_border_card.dart';
 
 void main() {
   runApp(StandaloneTestApp());
@@ -25,20 +24,18 @@ class StandaloneTestApp extends StatelessWidget {
           child: Column(
             children: [
               Expanded(
-                child: GreekVineBorderCard(
-                  body: EventPopupCard(
-                    avatarInitial: 'A',
-                    eventTitle: 'Charity Run',
-                    eventOwnerName: 'John Doe',
-                    eventOwnerEmail: 'john.doe@gmail.com',
-                    eventDescription:
-                        'Join us for a charity run to raise funds for cancer research. Every contribution counts!',
-                    assembledAmount: 1423,
-                    totalGoalAmount: 2000,
-                    growersCount: 242,
-                    benefitsText:
-                        'You will get a 10% discount on all products. Your contribution helps us make a difference!',
-                  ),
+                child: EventPopupCard(
+                  avatarInitial: 'A',
+                  eventTitle: 'Charity Run',
+                  eventOwnerName: 'John Doe',
+                  eventOwnerEmail: 'john.doe@gmail.com',
+                  eventDescription:
+                      'Join us for a charity run to raise funds for cancer research. Every contribution counts!',
+                  assembledAmount: 1423,
+                  totalGoalAmount: 2000,
+                  growersCount: 242,
+                  benefitsText:
+                      'You will get a 10% discount on all products. Your contribution helps us make a difference!',
                 ),
               ),
               ElevatedButton(
